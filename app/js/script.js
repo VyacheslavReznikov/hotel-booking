e2916474b5b3bd73e6e4ccdb64252ccb795d7197
VyacheslavReznikov <lycansth@list.ru>
2020-09-13 16:56:52 +1000

    added adaptive

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,27 @@
 $(function() {
 
+	new WOW().init();
 
-  new WOW().init();
+	$(document).scroll(function() {
+    let $nav = $(".header");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height())
+	})
+	
+	$('.menu__link').on('click', function() {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top + 'px'
+		}, {
+			duration: 1000,
+			easing: 'swing'
+		})
+		return false
+	})
+	
+	$('.burger').on('click', function() {
+		$('.burger').toggleClass('active');
+		$('.menu').toggleClass('active')
+		$('body').toggleClass('lock');
+	})
 
 
 })
